b1fa895fa103c7581215702ea56f0829437b1062
svitlanama <alexandrovna.sveta@gmail.com>
2018-06-17 16:44:12 +0300

    Initial project setup.

---
+++ tinder_dev/tinder_dev/Controller/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tinder_dev
+//
+//  Created by Svitlana Moiseyenko on 6/17/18.
+//  Copyright © 2018 svitlana. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+     
+    }
+
+
+}
+
